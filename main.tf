91701a57ed0c01373c8642a6ce3c053297de273b
Dominic P <dominic@mailtoforget.com>
2019-05-15 21:07:45 +1000

    Removed profile from AWS provider - users to use key and secret

--- main.tf
+++ main.tf
@@ -9,9 +9,8 @@ variable "myWorkloadTag" { default = "Session01-HelloWorld" }
 variable "aws_region" {default = "ap-southeast-2"}
 
 provider "aws" {
-#	access_key = "${var.aws_access_key}"
-#	secret_key = "${var.aws_secret_key}"
-	profile = "sandbox"
+	access_key = "${var.aws_access_key}"
+	secret_key = "${var.aws_secret_key}"
 	region	= "${var.aws_region}"
 }
 
